04423f193430660dbab61a526de0fec04efe084f
Durgam NISHIPRIYA <164764033+DurgamNishi@users.noreply.github.com>
2024-09-18 16:29:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./app.css";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div>
+        <h3 className="bg">Welcome to Events</h3>
+        <input type="text" />
+        <input />
+
+        <div className="EventCard">
+          <h3>add Event Details</h3>
+          <hr style={{ height: "1px", color: "white" }} />
+        </div>
+      </div>
+    </div>
+  );
+}
